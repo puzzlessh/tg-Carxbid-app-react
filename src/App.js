2a57961ac82e6f2601de1814e6e0e78192cf411e
puzzle <antonplatov@Antons-iMac.local>
2023-06-12 20:39:39 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { useEffect } from 'react';
 import './App.css';
-let tg = window.Telegram.WebApp;
+
 function App() {
   useEffect(() => {
     tg.ready();
   }, [])
 
-  const onClose = () => {
-    tg.close()
-  }
+
   return (
     <div className="App">
      <button onClick={onClose}>Закрыть</button>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "../button/button";
+
+const Header = () => {
+    let tg = window.Telegram.WebApp;
+    const onClose = () => {
+        tg.close()
+      }
+    
+    return( 
+    <div className={'header'}>
+        <Button onClick={onClose}>Закрыть</Button>
+        <span className={'username'}>
+            {tg.initDataUnsafe?.user?.username}
+        </span>
+    </div>
+    );
+
+}
+
+export default Header;  
