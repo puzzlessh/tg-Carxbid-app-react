1a8167a39f814a34c7d387de738018aa90e19448
puzzlessh <110547508+puzzlessh@users.noreply.github.com>
2023-11-12 21:55:57 +0300

    last change form Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,20 +4,7 @@ import { useTelegram } from '../../hooks/useTelegram';
 
 
 const Form = () => {
-  const [steps, setSteps] = useState([
-    'carMark',
-    'carModel',
-    'vin',
-    'caseNumber',
-    'insuranceType',
-    'carType',
-    'year',
-    'price',
-    'region',
-    'city',
-  ]);
   const [currentStep, setCurrentStep] = useState(0);
-
   const [formData, setFormData] = useState({
     carMark: '',
     carModel: '',
@@ -25,61 +12,80 @@ const Form = () => {
     caseNumber: '',
     insuranceType: '',
     carType: '',
-    year: '',
-    price: '',
+    year: 0,
+    price: 0,
     region: '',
     city: '',
   });
 
-  const handleInputChange = (fieldName, value) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [fieldName]: value,
-    }));
-  };
-
-  const handleNextStep = () => {
-    setCurrentStep((prevStep) => prevStep + 1);
-  };
+  const { tg } = useTelegram();
 
   const onSendData = useCallback(() => {
-    // Отправка данных
-    console.log('Отправка данных:', formData);
-  }, [formData]);
+    // Отправка данных на сервер
+    tg.sendData(JSON.stringify(formData));
+    // Сброс данных и переход на первый шаг
+    setFormData({
+      carMark: '',
+      carModel: '',
+      vin: '',
+      caseNumber: '',
+      insuranceType: '',
+      carType: '',
+      year: 0,
+      price: 0,
+      region: '',
+      city: '',
+    });
+    setCurrentStep(0);
+  }, [formData, tg]);
+
+  const onNextStep = useCallback(() => {
+    setCurrentStep((prevStep) => prevStep + 1);
+  }, []);
+
+  const onChangeField = (fieldName, value) => {
+    setFormData((prevData) => ({ ...prevData, [fieldName]: value }));
+  };
+
+  const renderInput = (fieldName, placeholder) => (
+    <input
+      className={'input'}
+      type="text"
+      placeholder={placeholder}
+      value={formData[fieldName]}
+      onChange={(e) => onChangeField(fieldName, e.target.value)}
+    />
+  );
 
   useEffect(() => {
-    if (currentStep === steps.length) {
-      // Если текущий шаг равен количеству шагов, значит, все поля заполнены
-      // Показываем кнопку "Отправить данные"
-      // И отправляем данные
-      onSendData();
+    tg.onEvent('mainButtonClicked', currentStep === 10 ? onSendData : onNextStep);
+    return () => {
+      tg.offEvent('mainButtonClicked', currentStep === 10 ? onSendData : onNextStep);
+    };
+  }, [currentStep, onSendData, onNextStep]);
+
+  useEffect(() => {
+    if (!formData.carMark || !formData.carModel) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
     }
-  }, [currentStep, steps.length, onSendData]);
+  }, [formData.carMark, formData.carModel]);
 
   return (
     <div className={'form'}>
       <h3>Введите ваши данные</h3>
-      {steps.map((step, index) => (
-        <div key={step}>
-          {index === currentStep && (
-            <div>
-              <input
-                className={'input'}
-                type="text"
-                placeholder={`Введите ${step}`}
-                value={formData[step]}
-                onChange={(e) => handleInputChange(step, e.target.value)}
-              />
-              {index !== steps.length - 1 && (
-                <button onClick={handleNextStep}>Далее</button>
-              )}
-            </div>
-          )}
-        </div>
-      ))}
-      {currentStep === steps.length && (
-        <button onClick={onSendData}>Отправить данные</button>
-      )}
+      {currentStep === 0 && renderInput('carMark', 'Марка транспортного средства:')}
+      {currentStep === 1 && renderInput('carModel', 'Модель транспортного средства:')}
+      {currentStep === 2 && renderInput('vin', 'VIN:')}
+      {currentStep === 3 && renderInput('caseNumber', 'FRAME:')}
+      {currentStep === 4 && renderInput('insuranceType', 'Мощность л/с:')}
+      {currentStep === 5 && renderInput('carType', 'Тип КПП:')}
+      {currentStep === 6 && renderInput('year', 'Год выпуска:')}
+      {currentStep === 7 && renderInput('price', 'Цена:')}
+      {currentStep === 8 && renderInput('region', 'Регион:')}
+      {currentStep === 9 && renderInput('city', 'Город:')}
+      {currentStep === 10 && <button onClick={onSendData}>Отправить данные</button>}
     </div>
   );
 };
