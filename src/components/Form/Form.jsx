7a558dd0b259a958c02ea6349961e3d32882d664
puzzlessh <110547508+puzzlessh@users.noreply.github.com>
2023-11-12 22:22:07 +0300

    test react Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,9 +2,10 @@ import React, { useCallback, useEffect, useState } from 'react';
 import './Form.css';
 import { useTelegram } from '../../hooks/useTelegram';
 
-
 const Form = () => {
-  const [steps, setSteps] = useState([
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const steps = [
     'carMark',
     'carModel',
     'vin',
@@ -15,8 +16,7 @@ const Form = () => {
     'price',
     'region',
     'city',
-  ]);
-  const [currentStep, setCurrentStep] = useState(0);
+  ];
 
   const [formData, setFormData] = useState({
     carMark: '',
@@ -31,6 +31,8 @@ const Form = () => {
     city: '',
   });
 
+  const { tg } = useTelegram();
+
   const handleInputChange = (fieldName, value) => {
     setFormData((prevData) => ({
       ...prevData,
@@ -39,47 +41,59 @@ const Form = () => {
   };
 
   const handleNextStep = () => {
-    setCurrentStep((prevStep) => prevStep + 1);
+    if (currentStep < steps.length - 1) {
+      setCurrentStep((prevStep) => prevStep + 1);
+    }
+  };
+
+  const handlePrevStep = () => {
+    if (currentStep > 0) {
+      setCurrentStep((prevStep) => prevStep - 1);
+    }
   };
 
   const onSendData = useCallback(() => {
     // Отправка данных
     console.log('Отправка данных:', formData);
+    // Здесь может быть ваша логика отправки данных
   }, [formData]);
 
   useEffect(() => {
-    if (currentStep === steps.length) {
-      // Если текущий шаг равен количеству шагов, значит, все поля заполнены
-      // Показываем кнопку "Отправить данные"
-      // И отправляем данные
-      onSendData();
-    }
-  }, [currentStep, steps.length, onSendData]);
+    tg.onEvent('mainButtonClicked', handleNextStep);
+    return () => {
+      tg.offEvent('mainButtonClicked', handleNextStep);
+    };
+  }, [handleNextStep, tg]);
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: currentStep === steps.length - 1 ? 'Отправить данные' : 'Далее',
+    });
+  }, [currentStep, steps.length, tg]);
 
   return (
     <div className={'form'}>
       <h3>Введите ваши данные</h3>
-      {steps.map((step, index) => (
-        <div key={step}>
-          {index === currentStep && (
-            <div>
-              <input
-                className={'input'}
-                type="text"
-                placeholder={`Введите ${step}`}
-                value={formData[step]}
-                onChange={(e) => handleInputChange(step, e.target.value)}
-              />
-              {index !== steps.length - 1 && (
-                <button onClick={handleNextStep}>Далее</button>
-              )}
-            </div>
-          )}
-        </div>
-      ))}
-      {currentStep === steps.length && (
-        <button onClick={onSendData}>Отправить данные</button>
-      )}
+      <div>
+        <input
+          className={'input'}
+          type="text"
+          placeholder={`Введите ${steps[currentStep]}`}
+          value={formData[steps[currentStep]]}
+          onChange={(e) => handleInputChange(steps[currentStep], e.target.value)}
+        />
+      </div>
+      <div>
+        {currentStep > 0 && (
+          <button onClick={handlePrevStep}>Назад</button>
+        )}
+        {currentStep < steps.length - 1 && (
+          <button onClick={handleNextStep}>Далее</button>
+        )}
+        {currentStep === steps.length - 1 && (
+          <button onClick={onSendData}>Отправить данные</button>
+        )}
+      </div>
     </div>
   );
 };
