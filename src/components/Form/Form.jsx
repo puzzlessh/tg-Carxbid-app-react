e10dc108d373ce8ca00afd34af07e504bd1d2bae
puzzlessh <110547508+puzzlessh@users.noreply.github.com>
2023-11-12 20:00:55 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,209 +1,218 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './Form.css';
-import {useTelegram} from "../../hooks/useTelegram";
+import { useTelegram } from '../../hooks/useTelegram';
 
 const Form = () => {
-    const [mark, setMark] = useState('');
-    const [model, setModel] = useState('');
-    const [vin, setVin] = useState('');
-    const [frame, setFrame] = useState('');
-    const [year, setYear] = useState('');
-    const [lsauto, setLsauto] = useState('');
-    const [kpp, setKpp] = useState('');
-    const [probeg, setProbeg] = useState('');
-    const [region, setRegion] = useState('');
-    const [city, setCity] = useState('');
-    const [desc, setDesc] = useState('');
-    const [minprice, setMinprice] = useState('');
+  const [auctionType, setAuctionType] = useState('');
+  const [bindingOfferTime, setBindingOfferTime] = useState(15);
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
+  const [price, setPrice] = useState(0);
+  const [region, setRegion] = useState('');
+  const [city, setCity] = useState('');
+  const [caseNumber, setCaseNumber] = useState('');
+  const [auctionObjectType, setAuctionObjectType] = useState('');
 
-    //const [subject, setSubject] = useState('physical');
-    const {tg} = useTelegram();
+  const [insuranceType, setInsuranceType] = useState('');
+  const [vin, setVin] = useState('');
+  const [year, setYear] = useState(0);
+  const [carType, setCarType] = useState('');
+  const [carMark, setCarMark] = useState('');
+  const [carModel, setCarModel] = useState('');
 
-    const onSendData = useCallback(() => {
-        const data = {
-            mark,
-            model,
-            vin,
-            frame,
-            year,
-            lsauto,
-            kpp,
-            probeg,
-            region,
-            city,
-            desc,
-            minprice
-        }
-        tg.sendData(JSON.stringify(data));
-    }, [mark, model, vin, frame, year, lsauto, kpp, probeg, region, city, desc, minprice])
+  const { tg } = useTelegram();
 
-    useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
+  const onSendData = useCallback(() => {
+    const data = {
+      auction_type: auctionType,
+      binding_offer_time: bindingOfferTime,
+      start,
+      end,
+      price,
+      region,
+      city,
+      case_number: caseNumber,
+      auction_object_type: auctionObjectType,
+      auction_object: {
+        insurance_type: insuranceType,
+        vin: {
+          type: 'vin',
+          value: vin,
+        },
+        year,
+        car_type: {
+          type: 'text',
+          value: carType,
+        },
+        car_mark: {
+          type: 'text',
+          value: carMark,
+        },
+        car_model: {
+          type: 'text',
+          value: carModel,
+        },
+      },
+    };
 
-    useEffect(() => {
-        tg.MainButton.setParams({
-            text: 'Отправить данные'
-        })
-    }, [])
+    tg.sendData(JSON.stringify(data));
+  }, [
+    auctionType,
+    bindingOfferTime,
+    start,
+    end,
+    price,
+    region,
+    city,
+    caseNumber,
+    auctionObjectType,
+    insuranceType,
+    vin,
+    year,
+    carType,
+    carMark,
+    carModel,
+  ]);
 
-    useEffect(() => {
-        if(!mark || !model) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    }, [mark, model])
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData);
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData);
+    };
+  }, [onSendData]);
 
-    const onChangeModel= (e) => {
-        setModel(e.target.value)
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: 'Отправить данные',
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!carMark || !carModel) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
     }
-    const onChangeVin = (e) => {
-        setVin(e.target.value)
-    }
+  }, [carMark, carModel]);
 
-    const onChangeMark = (e) => {
-        setMark(e.target.value)
-    }
+  const onChangeModel = (e) => {
+    setCarModel(e.target.value);
+  };
+  const onChangeVin = (e) => {
+    setVin(e.target.value);
+  };
 
-    const onChangeFrame = (e) => {
-        setFrame(e.target.value)
-    }
+  const onChangeMark = (e) => {
+    setCarMark(e.target.value);
+  };
 
-    const onChangeYear = (e) => {
-        setYear(e.target.value)
-    }
+  const onChangeYear = (e) => {
+    setYear(e.target.value);
+  };
 
-    const onChangeLsauto = (e) => {
-        setLsauto(e.target.value)
-    }
+  const onChangeRegion = (e) => {
+    setRegion(e.target.value);
+  };
+  const onChangeCity = (e) => {
+    setCity(e.target.value);
+  };
+  const onChangeDesc = (e) => {
+    setCaseNumber(e.target.value);
+  };
 
-    const onChangeKpp = (e) => {
-        setKpp(e.target.value)
-    }
+  const onChangeMinprice = (e) => {
+    setPrice(e.target.value);
+  };
 
-    const onChangeProbeg = (e) => {
-        setProbeg(e.target.value)
-    }
-
-    const onChangeRegion = (e) => {
-        setRegion(e.target.value)
-    }
-    const onChangeCity = (e) => {
-        setCity(e.target.value)
-    }
-    const onChangeDesc = (e) => {
-        setDesc(e.target.value)
-    }
-
-    const onChangeMinprice = (e) => {
-        setMinprice(e.target.value)
-    }
-
-    // const onChangeStreet = (e) => {
-    //     setStreet(e.target.value)
-    // }
-
-    // const onChangeSubject = (e) => {
-    //     setSubject(e.target.value)
-    // }
-
-    return (
-        <div className={"form"}>
-            <h3>Введите ваши данные</h3>
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Марка транспортного средства:'}
-                value={mark}
-                onChange={onChangeMark}
-            />
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Модель транспортного средства:'}
-                value={model}
-                onChange={onChangeModel}
-            />
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'VIN:'}
-                value={vin}
-                onChange={onChangeVin}
-            />
-                        <input
-                className={'input'}
-                type="text"
-                placeholder={'FRAME:'}
-                value={frame}
-                onChange={onChangeFrame}
-            />
-                        <input
-                className={'input'}
-                type="text"
-                placeholder={'Мощность л/с:'}
-                value={lsauto}
-                onChange={onChangeLsauto}
-            />
-                        <input
-                className={'input'}
-                type="text"
-                placeholder={'Тип КПП:'}
-                value={kpp}
-                onChange={onChangeKpp}
-            />
-                        <input
-                className={'input'}
-                type="text"
-                placeholder={'Год выпуска:'}
-                value={year}
-                onChange={onChangeYear}
-            />
-                        <input
-                className={'input'}
-                type="text"
-                placeholder={'Пробег:'}
-                value={probeg}
-                onChange={onChangeProbeg}
-            />
-                        <input
-                className={'input'}
-                type="text"
-                placeholder={'Регион:'}
-                value={region}
-                onChange={onChangeRegion}
-            />
-                        <input
-                className={'input'}
-                type="text"
-                placeholder={'Город:'}
-                value={city}
-                onChange={onChangeCity}
-            />
-                        <input
-                className={'input'}
-                type="text"
-                placeholder={'Дополнительная информация о ТС:'}
-                value={desc}
-                onChange={onChangeDesc}
-            />
-                        <input
-                className={'input'}
-                type="text"
-                placeholder={'Желаемая минимальная стоимость:'}
-                value={minprice}
-                onChange={onChangeMinprice}
-            />
-            {/* <select value={subject} onChange={onChangeSubject} className={'select'}>
-                <option value={'physical'}>Физ. лицо</option>
-                <option value={'legal'}>Юр. лицо</option>
-            </select> */}
-        </div>
-    );
+  return (
+    <div className={'form'}>
+      <h3>Введите ваши данные</h3>
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Марка транспортного средства:'}
+        value={carMark}
+        onChange={onChangeMark}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Модель транспортного средства:'}
+        value={carModel}
+        onChange={onChangeModel}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'VIN:'}
+        value={vin}
+        onChange={onChangeVin}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'FRAME:'}
+        value={caseNumber}
+        onChange={onChangeDesc}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Мощность л/с:'}
+        value={insuranceType}
+        onChange={(e) => setInsuranceType(e.target.value)}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Тип КПП:'}
+        value={carType}
+        onChange={(e) => setCarType(e.target.value)}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Год выпуска:'}
+        value={year}
+        onChange={onChangeYear}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Пробег:'}
+        value={price}
+        onChange={onChangeMinprice}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Регион:'}
+        value={region}
+        onChange={onChangeRegion}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Город:'}
+        value={city}
+        onChange={onChangeCity}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Дополнительная информация о ТС:'}
+        value={start}
+        onChange={(e) => setStart(e.target.value)}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Желаемая минимальная стоимость:'}
+        value={end}
+        onChange={(e) => setEnd(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Form;
